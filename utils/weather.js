d31d5b39087d854964113b40dd7633200c5e138d
tr15tan01 <t.varamashvili@gmail.com>
2022-12-25 19:14:38 +0400

    added nav and footer and refactored for deployment

--- utils/weather.js
+++ utils/weather.js
@@ -1,19 +1,19 @@
-const axios = require('axios')
-const currentWeatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q='
-const forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast?q='
-const key = process.env.KEY
+const axios = require("axios");
+const currentWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
+const forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?q=";
+const key = process.env.KEY;
 
-async function getWeather(cityID = 'chicago', url) {
-    try {
-        let res = await axios.get(url + cityID + "&appid=" + key)
-        let data = res.data
-        return data
-    } catch (error) {
-        let res = await axios.get(url + 'chicago' + "&appid=" + key)
-        let data = res.data
-        data.additional = `City - ${cityID} - not found, try another place...`
-        return data
-    }
+async function getWeather(cityID = "chicago", url) {
+  try {
+    let res = await axios.get(url + cityID + "&appid=" + key);
+    let data = res.data;
+    return data;
+  } catch (error) {
+    let res = await axios.get(url + "chicago" + "&appid=" + key);
+    let data = res.data;
+    data.additional = `City - ${cityID} - not found, try another place...`;
+    return data;
+  }
 }
 
-module.exports = { currentWeatherUrl, forecastUrl, getWeather }
+module.exports = { currentWeatherUrl, forecastUrl, getWeather };
